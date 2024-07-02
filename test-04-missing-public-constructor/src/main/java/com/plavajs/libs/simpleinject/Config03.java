08fb27179a5cf72e72987a1f19dbe34bd720e93d
plavajs <mlnarik.lukas@seznam.cz>
2024-07-02 12:53:58 +0200

    0001: duplicit & MissingPublicConstructorException tests

---
+++ test-04-missing-public-constructor/src/main/java/com/plavajs/libs/simpleinject/Config03.java
@@ -0,0 +1,35 @@
+package com.plavajs.libs.simpleinject;
+
+import com.plavajs.libs.simpleinject.a.A1;
+import com.plavajs.libs.simpleinject.a.A2;
+import com.plavajs.libs.simpleinject.annotation.SimpleBean;
+import com.plavajs.libs.simpleinject.annotation.SimpleComponentScan;
+import com.plavajs.libs.simpleinject.annotation.SimpleConfiguration;
+import com.plavajs.libs.simpleinject.b.B1;
+import com.plavajs.libs.simpleinject.c.C1;
+import com.plavajs.libs.simpleinject.d.D1;
+
+@SimpleConfiguration
+@SimpleComponentScan(packages = "com.plavajs.libs.simpleinject")
+public class Config03 {
+
+    @SimpleBean(identifier = "simple-bean-A1")
+    public static A1 simpleBeanA1(A2 a2) {
+        return new A1(a2);
+    }
+
+    @SimpleBean(identifier = "simple-bean-B1")
+    public static B1 simpleBeanB1() {
+        return new B1();
+    }
+
+    @SimpleBean(identifier = "simple-bean-C1")
+    public static C1 simpleBeanC1() {
+        return new C1();
+    }
+
+    @SimpleBean(identifier = "simple-bean-D1")
+    public static D1 simpleBeanD1() {
+        return new D1();
+    }
+}
--- test-04-missing-public-constructor/src/main/java/com/plavajs/libs/simpleinject/c/C2.java
+++ test-04-missing-public-constructor/src/main/java/com/plavajs/libs/simpleinject/c/C2.java
@@ -1,4 +1,4 @@
-package com.plavajs.libs.simpleinject.d;
+package com.plavajs.libs.simpleinject.c;
 
 import com.plavajs.libs.simpleinject.annotation.SimpleComponent;
 import com.plavajs.libs.simpleinject.annotation.SimpleInject;
@@ -6,6 +6,15 @@ import lombok.Getter;
 
 @Getter
 @SimpleComponent
-public class D5 {
+public class C2 {
+
+    private C3 c3;
+
+    @SimpleInject
+    private C4 c4;
+
+    public C2(C3 c3) {
+        this.c3 = c3;
+    }
 
 }
